8c928cc4cfe3f206d3596aa573966924e2d975b7
RonitKumar09 <ronitkumar.robot@gmail.com>
2022-03-27 16:58:27 +0530

    Initial Setup

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+function NavBar() {
+  return (
+    <header>
+      <div>
+        <div className="logo">logo</div>
+        <div className="searchbar">searchBar</div>
+      </div>
+      <div>
+        <ul>
+          <li>option</li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default NavBar;
