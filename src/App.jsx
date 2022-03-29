d89410e9531e72f4041c2e4ee5778e23debb488a
RonitKumar09 <ronitkumar.robot@gmail.com>
2022-03-29 21:29:21 +0530

    Add Router File

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch></Switch>
-      </Router>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
